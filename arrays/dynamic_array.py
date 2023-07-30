3f9f98171ebbf72490174e98c98ce7a7911a6c3e
Junseo Yang <junseo.yang@bell.ca>
2023-07-30 12:13:07 -0400

    Add resize, append, and get_item in DynamicArray

--- arrays/dynamic_array.py
+++ arrays/dynamic_array.py
@@ -23,9 +23,30 @@ class DynamicArray(object):
             return True
         else:
             return False
+    def _resize(self, new_cap):
+        B = self.ma
+    def append(self, e):
+        """
+        Add an element to the end of the array
+        """
+        if self.n == self.capacity:
+
+        self.A[self.n] = e
+        self.n += 1
+    def __get_item__(self, i):
+        """
+        Returns an element at index i
+        """
+        if not 0 <= i < self.n:
+            return IndexError(f'index {i} is out of bounds')
+        return self.A[i]
+    
 da = DynamicArray()
 
 # len
 print(len(da))
 
+# is_empty
 print(da.is_empty())
+
+print(da.get_item())
